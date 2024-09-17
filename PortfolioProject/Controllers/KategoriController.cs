556915233f4073e372cd349cfd475a14701d0aa1
M.Baran Bilal <mahfuzbaranbilal@gmail.com>
2024-09-17 20:29:04 +0300

    Add project files.

---
+++ PortfolioProject/Controllers/KategoriController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        public ActionResult CategoryList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+    }
+}
